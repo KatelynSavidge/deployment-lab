d97f8f4ed60dc1df66ee67f148d97e4f85a5af2c
Katelyn Savidge <ktsavidge@gmail.com>
2022-11-02 17:10:50 -0500

    new

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const cors = require("cors")
-require("dotenv").config()
 
 
 const app = express()
 
-app.use(express.json())
-app.use(cors())
+// app.use(express.json())
+// app.use(cors())
 
 
 const {home} = require("./controllers/pageCtrl.js")
