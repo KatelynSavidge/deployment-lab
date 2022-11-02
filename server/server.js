87ef6d25eac1c82a45402adef8cffc1dae582d36
Katelyn Savidge <ktsavidge@gmail.com>
2022-11-02 15:34:06 -0500

    first

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+const {home} = require("./controllers/pageCtrl")
+
+
+app.get("/", home)
+
+
+
+
+const {PORT} =  process.env
+
+app.listen(PORT, () => console.log(`Server lightening on port ${PORT}`))
