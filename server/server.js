a65482157db478e132f4eda1dea2576abab21a01
Katelyn Savidge <ktsavidge@gmail.com>
2022-11-02 15:50:15 -0500

    third

--- server/server.js
+++ server/server.js
@@ -6,15 +6,13 @@ require("dotenv").config()
 const app = express()
 
 app.use(express.json())
-
 app.use(cors())
+
 
 const {home} = require("./controllers/pageCtrl")
 
 
 app.get("/", home)
-
-
 
 
 const {PORT} =  process.env
